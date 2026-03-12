810982ca29e905eaeeb4866800a8737757affc71
madhan-231105 <mathangopi23@gmail.com>
2026-03-12 18:34:48 +0530

    before cl

--- backend/models/Meal.js
+++ backend/models/Meal.js
@@ -5,7 +5,7 @@ const MealSchema = new mongoose.Schema({
     name: { type: String, required: true },
     type: { type: String, enum: ['breakfast', 'lunch', 'dinner', 'snack'], required: true },
     calories: { type: Number, required: true },
+    quantity: { type: String, required: true }, // Add this line
     date: { type: Date, default: Date.now }
 });
-
 module.exports = mongoose.model('Meal', MealSchema);
